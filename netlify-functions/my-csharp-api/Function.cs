af3e15893f5ae53a5fb42e76e1e013dada3c9b2f
Nijat Majidov <97800703+nicat827@users.noreply.github.com>
2024-06-30 02:44:28 +0400

    commit 2

---
+++ netlify-functions/my-csharp-api/Function.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using NetlifyFunctions;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NetlifyFunctions.Functions
+{
+    public class Handler
+    {
+        public async Task<APIGatewayProxyResponse> Run(APIGatewayProxyRequest request)
+        {
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } },
+                Body = "Hello from C# Netlify Function!",
+            };
+            return response;
+        }
+    }
+}
